b21a4b397b6e3d785485b56983f469b261e32d3b
Falduff <paddyffoley@gmail.com>
2022-04-27 17:06:14 +0100

    Code for histograms of each variable and number of unique variables in summary.txt

--- Analysis.py
+++ Analysis.py
@@ -26,12 +26,18 @@ lines7 = ['Summary of Variables','There are 5 variables in the Iris flower data 
 'Petal Length','This is measured from the middle of the flower to the end of the petal',
 '', 'Petal Width','This is a measurement across the widest part of the petal']
 
+lines8 = (df.nunique())
 
 with open('Summary.txt','w') as s:
     s.write("Description of the Data")
     s.write(2*"\n")
     s.write(str(lines))
     s.write(2*"\n")
+    s.write("Unique Variables in each Column")
+    s.write("\n")
+    s.write(str(lines8))
+    s.write(2*"\n")
+
     s.write("First 5 rows of the Dataset")
     s.write(2*"\n")
     s.write(str(lines2))
@@ -50,3 +56,45 @@ with open('Summary.txt','w') as s:
     s.write("\n")
     s.write("\n")
     s.write('\n'.join(lines7))
+
+#2 Histogram for each variable
+
+#Sepal Length Histogram
+plt.hist(df['sepal.length'], bins=30)
+plt.title("Distribution of Sepal Length",fontsize = 15)
+plt.xlabel("Sepal Length in cm", fontsize = 10)
+plt.ylabel("Frequency of Species", fontsize = 10)
+plt.savefig('sepal_length.png')
+plt.clf()
+
+#Sepal Width Histogram
+plt.hist(df['sepal.width'], bins=30)
+plt.title("Distribution of Sepal Width",fontsize = 15)
+plt.xlabel("Sepal Width in cm", fontsize = 10)
+plt.ylabel("Frequency of Species", fontsize = 10)
+plt.savefig('sepal_width.png')
+plt.clf()
+
+#Petal Length Histogram
+plt.hist(df['petal.length'], bins=30)
+plt.title("Distribution of Petal Length",fontsize = 15)
+plt.xlabel("Petal Length in cm", fontsize = 10)
+plt.ylabel("Frequency of Species", fontsize = 10)
+plt.savefig('petal_length.png')
+plt.clf()
+
+#Petal Width Histogram
+plt.hist(df['petal.width'], bins=30)
+plt.title("Distribution of Petal Width",fontsize = 15)
+plt.xlabel("Petal Width in cm", fontsize = 10)
+plt.ylabel("Frequency of Species", fontsize = 10)
+plt.savefig('petal_width.png')
+plt.clf()
+
+#Variety of Species Histogram
+plt.hist(df['variety'], bins=30)
+plt.title("Distribution of Species",fontsize = 15)
+plt.xlabel("Species Name", fontsize = 10)
+plt.ylabel("Frequency of Species", fontsize = 10)
+plt.savefig('variety.png')
+plt.clf()
